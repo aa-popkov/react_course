7cef1b6d8ba39b66f1086ca24c29f81563cc8cbb
Alexey Popkov <AlexeyPopkov@pm.me>
2023-06-04 21:13:17 +0300

    Add classNames custom func

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { Route, Routes } from "react-router-dom"
 import { Link } from "react-router-dom"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
-import { Theme, ThemeContext } from "./theme/ThemeContext"
 import { useTheme } from "./theme/useTheme"
+import { classNames } from "./helpers/classNames/classNames"
 
 export const App = () => {
-  const {theme, toggleTheme} = useTheme()
-  
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Change theme</button>
       <Link to={"/"}>MainPage</Link>
       <Link to={"/about"}>AboutPage</Link>
