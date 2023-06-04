f398adcbb7516ae6f3b6f6e3770e1ba04ffa0af7
Alexey Popkov <AlexeyPopkov@pm.me>
2023-06-04 20:50:52 +0300

    Add style, theme. Work with context, provider.

--- build/src_pages_AboutPage_AboutPage_tsx.5528ccf839bdbc3de228.js
+++ build/src_pages_AboutPage_AboutPage_tsx.5528ccf839bdbc3de228.js
@@ -9,16 +9,17 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   AboutPage: () => (/* binding */ AboutPage)
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 
 var AboutPage = function () {
     return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "AboutPage" }));
 };
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
 
 
 /***/ })
 
 }]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LjVjYmE1YmZiYzZhYTNiMGMzNjk3LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ3pDO0FBQ1AsWUFBWSxzREFBSSxVQUFVLHVCQUF1QjtBQUNqRCIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWN0X2NvdXJzZS8uL3NyYy9wYWdlcy9BYm91dFBhZ2UvQWJvdXRQYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xuZXhwb3J0IHZhciBBYm91dFBhZ2UgPSBmdW5jdGlvbiAoKSB7XG4gICAgcmV0dXJuIChfanN4KFwiZGl2XCIsIHsgY2hpbGRyZW46IFwiQWJvdXRQYWdlXCIgfSkpO1xufTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LjU1MjhjY2Y4MzliZGJjM2RlMjI4LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHVCQUF1QjtBQUNqRDtBQUNBLGlFQUFlLFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWN0X2NvdXJzZS8uL3NyYy9wYWdlcy9BYm91dFBhZ2UvQWJvdXRQYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xudmFyIEFib3V0UGFnZSA9IGZ1bmN0aW9uICgpIHtcbiAgICByZXR1cm4gKF9qc3goXCJkaXZcIiwgeyBjaGlsZHJlbjogXCJBYm91dFBhZ2VcIiB9KSk7XG59O1xuZXhwb3J0IGRlZmF1bHQgQWJvdXRQYWdlO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
