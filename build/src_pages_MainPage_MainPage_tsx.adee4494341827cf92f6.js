595e8b1ef6b6048b131f875fe1ed1a899ddf35f9
Alexey Popkov <AlexeyPopkov@pm.me>
2023-06-04 19:49:55 +0300

    Add lazy-loading

---
+++ build/src_pages_MainPage_MainPage_tsx.adee4494341827cf92f6.js
@@ -0,0 +1,24 @@
+"use strict";
+(self["webpackChunkreact_course"] = self["webpackChunkreact_course"] || []).push([["src_pages_MainPage_MainPage_tsx"],{
+
+/***/ "./src/pages/MainPage/MainPage.tsx":
+/*!*****************************************!*\
+  !*** ./src/pages/MainPage/MainPage.tsx ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   MainPage: () => (/* binding */ MainPage)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var MainPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "MainPage" }));
+};
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX01haW5QYWdlX01haW5QYWdlX3RzeC5hZGVlNDQ5NDM0MTgyN2NmOTJmNi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFnRDtBQUN6QztBQUNQLFlBQVksc0RBQUksVUFBVSxzQkFBc0I7QUFDaEQiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWFjdF9jb3Vyc2UvLi9zcmMvcGFnZXMvTWFpblBhZ2UvTWFpblBhZ2UudHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGpzeCBhcyBfanN4IH0gZnJvbSBcInJlYWN0L2pzeC1ydW50aW1lXCI7XG5leHBvcnQgdmFyIE1haW5QYWdlID0gZnVuY3Rpb24gKCkge1xuICAgIHJldHVybiAoX2pzeChcImRpdlwiLCB7IGNoaWxkcmVuOiBcIk1haW5QYWdlXCIgfSkpO1xufTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Counter } from './components/Counter'
-import './index.scss'
+import React, { Suspense } from "react"
+import "./index.scss"
+import { Route, Routes } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
+import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 
 export const App = () => {
   return (
-    <div className='app'>
-      asdasda
-      <Counter/>
+    <div className="app">
+      <Link to={"/"}>MainPage</Link>
+      <Link to={"/about"}>AboutPage</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
